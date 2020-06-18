bcb5d0035e02133bc69e2ce1519449480b13f2de
jellynews <67012241+jellynews@users.noreply.github.com>
2020-06-18 17:13:08 +0800

    Add files via upload

---
+++ taiwan/taiwan.js
@@ -0,0 +1,203 @@
+	var place_data = [{
+	        tag: "Taipei City",
+	        num: "",
+	        place: "臺北市",
+			news_title:"千人扮裝遊行　盼大眾看見跨性別",
+			explain:"【記者陳子瑜台北報導】「我們要讓大家看到，跨性別族群在這裡！」近2000人的遊行隊伍⋯⋯",
+			img:"跨性別.png",
+			website: "../transgender/transgender.html"
+	    },
+	    {
+	        tag: "New Taipei City",
+	        num: "",
+			place: "新北市",
+			news_title:"屢遭校方漠視　北大翻牆社升彩虹旗傳訴求",
+			explain: "【記者陳子瑜綜合報導】刺耳喇叭聲交錯響徹，原是校慶開幕儀式的場合忽然被打斷，一群⋯⋯",
+			img:"彩虹旗.jpg",
+			website:"../rainbow/rainbow.html"
+	    },
+	    {
+	        tag: "Taichung City",
+	        num: "",
+	        place: "台中市",
+	        explain: ""
+	    },
+	    {
+	        tag: "Tainan City",
+	        num: "",
+			place: "臺南市",
+			news_title:"融反毒知識於機器人競賽　嘉藥翻轉制式化宣導",
+			explain: "【記者呂心喻台南報導】裁判一聲令下，現場500到600位參賽同學拿起機器人放在軌道上⋯⋯",
+			img:"機器人.jpg",
+			website:"../tainan/tainan.html"
+	    },
+	    {
+	        tag: "Kaohsiung City",
+	        num: "",
+			place: "高雄市",
+			news_title:"大作「紋」章　亞洲刺青博覽會青年新血嶄頭角",
+			explain: "【記者方小瑀高雄報導】「提筆刺下去，就是一個人的一輩子。」亞洲最大型刺青展⋯⋯"
+			,img:"刺青.jpg",
+			website:"../KH/KH.html"
+			
+	    },
+	    {
+	        tag: "Keelung City",
+	        num: "",
+	        place: "基隆市",
+	        explain: ""
+	    },
+	    {
+	        tag: "Taoyuan Country",
+			num: "",
+			news_title:"精油舒壓推筋轉脈　學生芳療賽獻「手」藝",
+	        place: "桃園市",
+			explain: "【記者陳妍如桃園報導】轉開精油瓶蓋，酸甜香氣在空氣中迸發開來，讓人分不清是⋯⋯",
+			img:"芳療.jpg",
+			website:"../taoyuan/taoyuan.html"
+			
+			
+	    },
+	    {
+	        tag: "Hsinchu City",
+	        num: "",
+	        place: "新竹市",
+	        explain: ""
+	    },
+	    {
+	        tag: "Hsinchu Country",
+	        num: "",
+	        place: "新竹縣",
+	        explain: ""
+	    },
+	    {
+	        tag: "Miaoli Country",
+	        num: "",
+	        place: "苗栗縣",
+	        explain: ""
+	    },
+	    {
+	        tag: "Changhua Country",
+			num: "",
+			news_title:"彰縣推海牛文化節　邀民體驗牛車採蚵",
+	        place: "彰化縣",
+			explain: "【記者方小瑀彰化報導】結實壯碩的「海牛」緩慢向前行，牛蹄一步步踏進濕軟的潮間帶⋯⋯",
+			img:"民眾坐牛車.jpg",
+			website:"../seahorse/seahorse.html"
+			
+	    },
+	    {
+	        tag: "Nantou Country",
+			num: "",
+			news_title:"揮別九二一陰霾　搶成功展客家文化",
+	        place: "南投縣",
+			explain: "【記者萬巧蓉南投報導】參賽者推動巨大的船型三輪車，一路奔馳闖到終點，拔下「搶成功」⋯⋯",
+			img:"搶成功.jpg",
+			website:"../nantou/nantou.html"
+	    },
+	    {
+	        tag: "Yunlin Country",
+			num: "",
+			news_title:"【專欄】百台無人機齊升天　虎尾科大團隊創另類夜景",
+	        place: "雲林縣",
+			explain: "【記者呂心喻綜合報導】國立虎尾科技大學操場上，觀眾紛紛仰頭驚呼。夜空中，數十個光點⋯⋯",
+			img:"無人機.jpg",
+			website:"../yunlin/yunlin.html"
+	    },
+	    {
+	        tag: "Chiayi City",
+	        num: "",
+	        place: "嘉義市",
+	        explain: ""
+	    },
+	    {
+	        tag: "Chiayi Country",
+	        num: "",
+	        place: "嘉義縣",
+	        explain: ""
+	    },
+	    {
+	        tag: "Pingtung Country",
+	        num: "",
+	        place: "屏東縣",
+	        explain: ""
+	    },
+	    {
+	        tag: "Yilan Country",
+			num: "",
+			news_title:"創新湯頭打破傳統印象　宜蘭鴨肉「麵」目一新",
+	        place: "宜蘭縣",
+			explain: "【記者陳妍如宜蘭報導】鮮嫩的鴨肉鋪在起鍋不久的飽滿麵條上，會場中滷味、咖哩及味噌各⋯⋯",
+			img:"鴨肉.jpg",
+			website:"../Duck/duck.html"
+	    },
+	    {
+	        tag: "Hualien Country",
+			num: "",
+			news_title:"融入自然景致　大地時裝秀化舊車站為伸展台",
+	        place: "花蓮縣",
+			explain: "【記者萬巧蓉花蓮報導】時裝模特兒在車站月台的長道上迎風走秀，伸展台背景就是花東縱谷⋯⋯",
+			img:"走秀.jpg",
+			website:"../Hua/hua.html"
+	    },
+	    {
+	        tag: "Taitung Country",
+	        num: "",
+	        place: "台東縣",
+	        explain: ""
+	    },
+	    {
+	        tag: "Penghu Country",
+	        num: "",
+	        place: "澎湖縣",
+	        explain: ""
+	    },
+	    {
+	        tag: "Kinmen Country",
+	        num: "",
+	        place: "金門縣",
+	        explain: ""
+	    },
+	    {
+	        tag: "Lienchiang Country",
+	        num: "",
+	        place: "連江縣",
+	        explain: ""
+	    }
+	];
+
+	var vm = new Vue({
+	    el: "#TaiwanPig",
+	    data: {
+	        filter: "",
+	        place_data: place_data
+	    },
+	    computed: {
+	        now_area: function () {
+	            var vobj = this;
+	            var result = this.place_data.filter(function (obj) {
+	                return obj.tag == vobj.filter;
+	            });
+	            if (result.length == 0) {
+	                return null;
+	            }
+	            return result[0];
+	        }
+	    }
+	});
+
+	$("path").click(function (e) {
+		var tagname = $(this).attr("data-name");
+
+		vm.filter = tagname
+
+		var inputurl;
+		for(var i = 0; i < place_data.length; i++){
+			if(place_data[i].tag == tagname){
+				inputurl = place_data[i].website
+			}	
+		}
+		// console.log(inputurl)
+		document.getElementById("link").href = inputurl
+	});
+	
